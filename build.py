af6fb2cd565afe25cf53bbf3c35481dc5a2e4cf1
Gk0Wk <nmg_wk@yeah.net>
2021-08-30 20:00:04 +0800

    强化build轮子，能够编译生成nodejs插件包

--- build.py
+++ build.py
@@ -1,31 +1,70 @@
 # CodeMirror, copyright (c) by Marijn Haverbeke and others
 # Distributed under an MIT license: https://codemirror.net/LICENSE
-
 # Author: Gk0Wk (nmg_wk@yeah.net)
 # Licence: MIT
-if __name__ == '__main__':
-    import json
-    import os
+import re
+import os
+import json
+import shutil
 
-    with open('build.json', 'r') as fp:
-        build_info = json.loads(fp.read())
+SRC_DIR = './src'
+DIST_DIR = './dist'
 
+
+def gen_plugin_name(build_info):
+    return re.sub('[^A-Za-z0-9_]', '', build_info['plugin.info']
+                  ['name']) + '-' + build_info['plugin.info']['version']
+
+
+def build_json_plugin(build_info):
     tiddlers = {}
-    for tiddler_ in build_info['shadow-tiddlers']:
-        tiddler = tiddler_['meta']
-        if tiddler_['type'] == 'external':
-            if os.path.exists(tiddler_['src']) and os.path.isfile(tiddler_['src']):
-                with open(tiddler_['src'], 'r') as fp:
-                    tiddler['text'] = fp.read()
-            else:
-                tiddler['text'] = ''
+    for tiddler_ in build_info['tiddlers']:
+        tiddler = tiddler_['fields']
+        file_path = os.path.join(SRC_DIR, tiddler_['src'])
+        if os.path.exists(file_path) and os.path.isfile(file_path):
+            with open(file_path, 'r') as ffp:
+                tiddler['text'] = ffp.read()
+        else:
+            tiddler['text'] = ''
         tiddlers[tiddler['title']] = tiddler
-    plugin = build_info['meta']
+    plugin = build_info['plugin.info']
     plugin['text'] = json.dumps({'tiddlers': tiddlers})
+    os.makedirs(DIST_DIR, exist_ok=True)
+    with open(os.path.join(DIST_DIR,
+                           gen_plugin_name(build_info) + '.json'), 'w') as ffp:
+        ffp.write(json.dumps([plugin]))
 
-    os.makedirs('./dist', exist_ok=True)
-    import re
-    with open('./dist/' +
-              re.sub('[^A-Za-z0-9_]', '', build_info['meta']['name'])
-              + '-' + build_info['meta']['version'] + '.json', 'w') as fp:
-        fp.write(json.dumps([plugin]))
+
+def build_node_plugin(build_info):
+    os.makedirs(os.path.join(DIST_DIR, 'node'), exist_ok=True)
+    with open(os.path.join(DIST_DIR, 'node', 'plugin.info'), 'w') as ffp:
+        ffp.write(json.dumps(build_info['plugin.info'], indent=4))
+    for tiddler in build_info['tiddlers']:
+        src_path = os.path.join(SRC_DIR, tiddler['src'])
+        if not os.path.exists(src_path) or not os.path.isfile(src_path):
+            continue
+        sub_dist_path = tiddler['dist'] if 'dist' in tiddler else tiddler[
+            'fields']['title'].split('/', 4)[-1]
+        dist_path = os.path.join(DIST_DIR, 'node', sub_dist_path)
+        os.makedirs(os.path.split(dist_path)[0], exist_ok=True)
+        shutil.copyfile(src_path, dist_path)
+        del tiddler['src']
+        if 'dist' in tiddler:
+            del tiddler['dist']
+        tiddler['file'] = sub_dist_path
+    with open(os.path.join(DIST_DIR, 'node', 'tiddlywiki.files'), 'w') as ffp:
+        ffp.write(json.dumps({'tiddlers': build_info['tiddlers']}, indent=4))
+    shutil.make_archive(os.path.join(
+        DIST_DIR,
+        gen_plugin_name(build_info),
+    ),
+                        format="zip",
+                        root_dir=os.path.join(DIST_DIR, 'node'))
+    shutil.rmtree(os.path.join(DIST_DIR, 'node'))
+
+
+if __name__ == '__main__':
+    with open('build.json', 'r') as fp:
+        build_info_str = fp.read()
+    build_json_plugin(json.loads(build_info_str))
+    build_node_plugin(json.loads(build_info_str))
