727b9ed814a70f613e0d2faca547a07e17f9cb91
Gk0Wk <Gk0Wk@users.noreply.github.com>
2021-11-01 17:40:27 +0000

    deploy: ec295ffc2a415a4faec8da3e7c7f8d515c768d44

--- service-worker.js
+++ service-worker.js
@@ -11,7 +11,7 @@ const { CacheFirst, StaleWhileRevalidate } = workbox.strategies;
 const { ExpirationPlugin } = workbox.expiration;
 const { precacheAndRoute, matchPrecache } = workbox.precaching;
 
-precacheAndRoute([{"revision":"51b1c635de81aaf49c1b674eb91971fa","url":"favicon.ico"},{"revision":"f027383aef0824dac225b8268e255e9c","url":"index.html"},{"revision":"ea2bab232dcff10a49b48a50be4f7c2e","url":"tiddlywiki5.js"}]);
+precacheAndRoute([{"revision":"51b1c635de81aaf49c1b674eb91971fa","url":"favicon.ico"},{"revision":"4d85aae5583f1ceb1c981617d7554749","url":"index.html"},{"revision":"ea2bab232dcff10a49b48a50be4f7c2e","url":"tiddlywiki5.js"}]);
 
 registerRoute(
   /\.css$/,
